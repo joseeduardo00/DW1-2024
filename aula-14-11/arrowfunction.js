ab6e7e884b58849a79b5df81efdbf9b5d5265909
José <163425218+joseeduardo00@users.noreply.github.com>
2024-11-14 11:50:30 -0300

    revisao_javascrpit

---
+++ aula-14-11/arrowfunction.js
@@ -0,0 +1,9 @@
+const saudacaoPadrao = () => console.log("Olá")
+const saudacao = nome => console.log(`Olá ${nome}`)
+
+const soma = (a,b) => Math.pow(a,b)
+
+
+saudacaoPadrao()
+saudacao("José")
+console.log(soma(2,3))
